dacbe25d4949b0f8f929fd430c469d973291431f
saivenkat <saivenkat.practice@gmail.com>
2026-05-02 00:36:13 +0530

    Add missing InstanceInformationService

---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/InstanceInformationService.java
@@ -0,0 +1,20 @@
+package com.in28minutes.microservices.currencyexchangeservice.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstanceInformationService {
+
+    private static final String HOST_NAME = "HOSTNAME";
+    private static final String DEFAULT_ENV = "Default";
+
+    @Autowired
+    private Environment environment;
+
+    public String retrieveInstanceInfo() {
+        return environment.getProperty(HOST_NAME, DEFAULT_ENV)
+                + " v2 ";
+    }
+}
